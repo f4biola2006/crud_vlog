33edc9ec1b417da20a04535f8260667d630a1fb1
fabiola <fabiolavrojas16@gmail.com>
2024-05-18 12:13:32 -0400

    post detail added

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 class BlogListView(ListView):
     model = Post 
     template_name = "home.html"
+    fields = titlle, author, body
+
+    class Blogdetailview(DetailView)
